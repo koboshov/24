9170519c67f90f768e2fa4ae70d451b5254c9986
User <koboshove@gmail.com>
2021-11-25 17:50:34 +0600

    home work24

---
+++ src/com/company/Main.java
@@ -0,0 +1,128 @@
+package com.company;
+
+import java.lang.reflect.Array;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        0 жана 1 деген сандар менен рандомно Массивди, ArrayListти жана LinkedListти толтурунуз.
+//                Overload методдорду тузунуз, алар сортировка болгон элементтерди кайтарсын. Башында 0дор жана аягында 1лер.
+
+
+//
+//        Random random = new Random();
+//        int[]array=new int[10];
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = random.nextInt(2);
+//            System.out.print(array[i]+" ");
+//        }
+//        counter(array);
+//        System.out.println("\nАрайлист");
+//        ArrayList<Integer>integers = new ArrayList<>();
+//        for (int i = 0; i < 10; i++) {
+//            int number = random.nextInt(2);
+//            integers.add(number);
+//            System.out.print(integers.get(i) + " ");
+//
+//        }
+//        counter(integers);
+//        System.out.println("\n");
+//        LinkedList<Integer>integers1 = new LinkedList<>();
+//        for (int i = 0; i < 10; i++) {
+//            int number2=random.nextInt(2);
+//            integers1.add(number2);
+//            System.out.print(integers1.get(i)+" ");
+//
+//
+//        }
+//        counter(integers1);
+//
+//    }
+//    public static void counter(int []array){
+//        Arrays.sort(array);
+//        System.out.println(Arrays.toString(array));
+//    }
+//    public static void counter(ArrayList<Integer>integers){
+//        Collections.sort(integers);
+//        System.out.println(integers);
+//    }
+//    public static void counter(LinkedList<Integer>integers1){
+//        Collections.sort(integers1);
+//        System.out.println(integers1);
+
+
+//        Random random = new Random();
+//        int array[] = new int[10];
+//        for (int i = 0; i < 10; i++) {
+//            array[i] = random.nextInt(2);
+//            System.out.println(array[i] + "");
+//        }
+//        counter(array);
+//        ArrayList<Integer> integers = new ArrayList<>();
+//        for (int i = 0; i < 10; i++) {
+//            int number = random.nextInt(2);
+//            integers.add(number);
+//            System.out.println(integers.get(i));
+//            .
+//        }...
+//        counter(integers);
+//        LinkedList<Integer> integers1 = new LinkedList<>();
+//        for (int i = 0; i < 10; i++) {
+//            int sum = random.nextInt();
+//            integers1.add(sum);
+//            System.out.println(integers1.get(i));
+//        }
+//        counter(integers1);
+//
+//
+//    }
+//
+//    public static void counter (int [] array){
+//        Arrays.sort(array);
+//        System.out.println(Arrays.toString(array));
+//
+//    }
+//    public static void counter (ArrayList<Integer>integers){
+//        Collections.sort(integers);
+//        System.out.println(integers);
+//    }
+//    public static void counter (ArrayList<Integer>integers1){
+//        Collections.sort(integers1);
+//        System.out.println(integers1);
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
